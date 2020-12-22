d6f1accd18122d29835d4fcb8e9cd435be220477
Efrain Nevarez <efrain.nevarezjr@gmail.com>
2020-12-21 20:44:03 -0600

    added basic schema

---
+++ schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS employeeTracker_DB;
+CREATE database employeeTracker_DB;
+
+USE employeeTracker_DB;
+
+CREATE TABLE departments(
+    id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE empRole(
+    id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL, 
+    departmen_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+
+SELECT * FROM empRole;
+
+SELECT * FROM employee;
