3cbdd03596e6f020dfac920e25058e019248fa7a
Efrain Nevarez <efrain.nevarezjr@gmail.com>
2020-12-23 21:37:58 -0600

    added employeetracker function to server.js

--- server.js
+++ server.js
@@ -4,7 +4,47 @@ const db = require("./db");
 
 require("console.table");
 
-const employeeTracker = () => {};
+const employeeTracker = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What would you like to do?",
+        choices: [
+          {
+            name: "View all Employees?",
+            value: "allEmployees",
+          },
+          {
+            name: "View all Roles?",
+            value: "allRoles",
+          },
+          {
+            name: "View all Departments?",
+            value: "allDepartments",
+          },
+          {
+            name: "Add a Department?",
+            value: "addDepartment",
+          },
+          {
+            name: "Add a Role?",
+            value: "addRole",
+          },
+          {
+            name: "Add an Employee?",
+            value: "addEmployees",
+          },
+          {
+            name: "Update and Employee?",
+            value: "updateEmployee",
+          },
+        ],
+      },
+    ])
+    .then();
+};
 
 const runDepartments = () => {};
 
