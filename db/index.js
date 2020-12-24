be41063367a768c5cb2b255b5231d1f0d60be55b
Efrain Nevarez <efrain.nevarezjr@gmail.com>
2020-12-23 21:13:37 -0600

    added find employee query selectors

--- db/index.js
+++ db/index.js
@@ -4,11 +4,22 @@ class DB {
   constructor(connection) {
     this.connection = connection;
   }
-  findEmployee() {}
-  findDepartment() {}
-  findRoles() {}
+  findEmployee() {
+    return this.connection.query(
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN empRole on employee.role_id = role.id LEFT JOIN departments on empRole.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id"
+    );
+  }
+
+  findDepartment() {
+    return this.connection.query("SELECT * departments");
+  }
+  findRoles() {
+    return this.connection.query("SELECT * empRole");
+  }
   createEmployee() {}
-  createDepartment() {}
+  createDepartment() {
+    return this.connection.query;
+  }
   createRole() {}
   updateEmployee() {}
 }
